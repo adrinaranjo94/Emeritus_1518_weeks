b8a6005773cae789ed1c2267418513713ecf1764
Adrián Naranjo <adriannaranjosanchez@gmail.com>
2022-11-07 16:51:16 +0100

    initial commit Emeritus Repository

---
+++ Week1/initialProject/index.js
@@ -0,0 +1,15 @@
+console.log("Hello world!");
+
+console.log("Hello world 2!");
+
+console.log("Hello World 3!");
+
+if (true) {
+  let index = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (arr[i]) {
+      let s = true;
+    }
+  }
+}
---
+++ Week2/arrays/index.js
@@ -0,0 +1,15 @@
+let fruits = ["apple", "banana", "pear"];
+
+console.log(fruits[0]); // apple
+console.log(fruits[4]); // undefined
+
+fruits.push("orange");
+
+console.log(fruits);
+// ["apple", "banana", "pear", "orange"]
+
+fruits.pop(); // Orange
+
+let newArray = [1, true, "apple"];
+
+console.log(newArray);
